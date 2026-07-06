ac128dc105b9c3bdd1f6f1b3b1b3735fa0e41542
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2026-07-06 23:30:17 +0630

    Add project files.

---
+++ BlazorStateSample/StateContainer.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorStateSample;
+
+public class StateContainer
+{
+    private int? counter;
+
+    public int Counter
+    {
+        get => counter ?? 0;
+        set
+        {
+            counter = value;
+            NotifyStateChanged();
+        }
+    }
+
+    public event Action? OnChange;
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
